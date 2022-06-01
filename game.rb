010531dc86e2e260e5753c012fc3c2273bf36c3a
olaleye <olaoduola01@gmail.com>
2022-06-01 08:31:37 +0100

    let yhou know if you have guessed write or wrong

--- game.rb
+++ game.rb
@@ -14,13 +14,32 @@ class Hangman
         ]
     end
 
-    def begin
-        puts "Your word has #{@word.first.length} letters"
-        puts "Lets get the game started. your clue is it #{@word.last}"
+    def tease
+        word_tease = ""
+
+        @word.first.length.times do
+            word_tease += "_ "
+        end
+        puts word_tease
+    end
+
+    def make_guess
         puts "enter a letter"
         guess = gets.chomp
 
-        puts "You guessed #{guess}"
+        right_choice = @word.first.include? guess
+        if right_choice
+            puts "You picked correct!"
+        else
+            puts "Your wrong try again."
+        end
+     end
+
+    def begin
+        puts "Your word has #{@word.first.length} letters"
+        tease
+        puts "Lets get the game started. your clue is it #{@word.last}"
+        make_guess
     end
 
     game = Hangman.new
