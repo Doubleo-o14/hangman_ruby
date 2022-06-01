addbb8d8fbc3bcd1e9e67cf449828bd3d9bf4efd
olaleye <olaoduola01@gmail.com>
2022-06-01 08:16:05 +0100

    displaying

--- game.rb
+++ game.rb
@@ -1,13 +1,30 @@
-class Hangman_game
+class Hangman
     def initialize 
-        letters = ('a'..'z').to_a
-        word = words.sample
+        @letters = ('a'..'z').to_a
+        @word = words.sample
     end
 
     def words
+        [
+            ["football", "most loved sport in europe"],
+            ["jogging", "not walking around"],
+            ["singing", "a form ofspeaking"],
+            ["education", "learning to improve your skill"],
+    
+        ]
     end
 
     def begin
+        puts "Your word has #{@word.first.length} letters"
+        puts "Lets get the game started. your clue is it #{@word.last}"
+        puts "enter a letter"
+        guess = gets.chomp
+
+        puts "You guessed #{guess}"
     end
 
+    game = Hangman.new
+    game.begin
+
 end
+
